5835bcc833171c057d0743313622323b2a8ceb3b
akshaypeekey <akshaypeekey@gmail.com>
2020-10-08 02:05:50 +0530

    backend asus 07/10

--- backend/urls.py
+++ backend/urls.py
@@ -19,9 +19,10 @@ from django.urls import path, include
 from backend.views import *
 
 urlpatterns = [
-    path('dashboard/', Home.as_view(), name='home'),
+    path('dashboard/', Home.as_view(), name='dashboard'),
     path('about/', About.as_view(), name='about'),
     path('vision/', Vision.as_view(), name='vision'),
     path('events/', Events.as_view(), name='events'),
     path('contact/', Contact.as_view(), name='contact'),
+
 ]
--- backend/views.py
+++ backend/views.py
@@ -4,6 +4,7 @@ from .forms import *
 from .models import *
 
 
+
 class Home(TemplateView):
     model = Index
     template_name = "dashboard.html"
@@ -18,7 +19,7 @@ class Home(TemplateView):
 
 class About(TemplateView):
     model = About
-    template_name = "web/dashboard.html"
+    template_name = "about.html"
     form_class = AboutForm
     def get(self, request, *args, **kwargs):
 
@@ -30,7 +31,7 @@ class About(TemplateView):
 
 class Vision(TemplateView):
     model = Vision
-    template_name = "web/vision.html"
+    template_name = "vision.html"
     form_class = VisionForm
     def get(self, request, *args, **kwargs):
 
@@ -42,7 +43,7 @@ class Vision(TemplateView):
 
 class Events(TemplateView):
     model = Events
-    template_name = "web/events.html"
+    template_name = "events.html"
     form_class = EventsForm
     def get(self, request, *args, **kwargs):
 
@@ -54,7 +55,7 @@ class Events(TemplateView):
 
 class Contact(TemplateView):
     model = Contact
-    template_name = "web/contact.html"
+    template_name = "contact.html"
     form_class = ContactForm
     def get(self, request, *args, **kwargs):
 
@@ -62,3 +63,4 @@ class Contact(TemplateView):
         context = {}
         context['form'] = form
         return render(request, self.template_name, context)
+
